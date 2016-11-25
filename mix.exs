de3824c1383b7bf687b093043153d7db9edae5b6
Aleksandr Aibulatov <zap.aibulatov@gmail.com>
2016-11-26 01:46:37 +0500

    Prepare dialyzer for work with phoenix

--- mix.exs
+++ mix.exs
@@ -9,6 +9,7 @@ defmodule Usd2rur.Mixfile do
      compilers: [:phoenix, :gettext] ++ Mix.compilers,
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
+     dialyzer: [plt_add_deps: :transitive],
      aliases: aliases(),
      deps: deps()]
   end
@@ -30,14 +31,16 @@ defmodule Usd2rur.Mixfile do
   #
   # Type `mix help deps` for examples and options.
   defp deps do
-    [{:phoenix, "~> 1.2.1"},
+    [{:phoenix, github: "aaronjensen/phoenix", ref: "suppress-dialyzer-warnings", override: true},
      {:phoenix_pubsub, "~> 1.0"},
      {:phoenix_ecto, "~> 3.0"},
      {:postgrex, ">= 0.0.0"},
      {:phoenix_html, "~> 2.6"},
      {:phoenix_live_reload, "~> 1.0", only: :dev},
      {:gettext, "~> 0.11"},
-     {:cowboy, "~> 1.0"}]
+     {:cowboy, "~> 1.0"},
+     {:dialyxir, "~> 0.3.5", only: [:dev]},
+     {:ecto, github: "aaronjensen/ecto", override: true}]
   end
 
   # Aliases are shortcuts or tasks specific to the current project.
