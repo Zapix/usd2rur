e4696a334c6aeef9dc3650e7d2b73d69a3c5282a
Aleksandr Aibulatov <zap.aibulatov@gmail.com>
2016-11-30 12:49:31 +0500

    Add tinkoff crawler

--- test/controllers/currency_controller_test.exs
+++ test/controllers/currency_controller_test.exs
@@ -78,6 +78,47 @@ defmodule Usd2rur.CurrencyControllerTest do
     end
   end
 
+  describe "/api/currency/tinkoff" do
+    test "success response", %{conn: conn} do
+      success_data = [
+        payload: [
+          rates: [
+            [
+              category: "DebitCardsTransfers",
+              fromCurrency: [code: 840],
+              toCurrency: [code: 643],
+              buy: 63.4,
+              sell: 64.4
+            ]
+          ]
+        ]
+      ]
+      {:ok, json} = JSON.encode(success_data)
+
+      response = %HTTPoison.Response{
+        status_code: 200,
+        body: json
+      }
+
+      with_mock HTTPoison, [get: fn _url -> {:ok, response} end] do
+        conn = get conn, "/api/currency/tinkoff"
+        assert json_response(conn, 200)
+      end
+    end
+
+    test "error response", %{conn: conn} do
+      response = %HTTPoison.Response{
+        status_code: 500,
+        body: "internal server error"
+      }
+
+      with_mock HTTPoison, [get: fn _url -> {:error, response} end] do
+        conn = get conn, "/api/currency/tinkoff"
+        assert json_response(conn, 400)
+      end
+    end
+  end
+
   test "not found", %{conn: conn} do
     conn = get conn, "/api/currency/fake-bank"
     assert json_response(conn, 404)
