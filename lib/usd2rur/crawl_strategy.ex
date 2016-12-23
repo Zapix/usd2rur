be708573739c1b7c71ca8c0f73ddbcda33a18934
Aleksandr Aibulatov <zap.aibulatov@gmail.com>
2016-12-23 15:02:40 +0700

    Use BankWorker in controllers

--- lib/usd2rur/crawl_strategy.ex
+++ lib/usd2rur/crawl_strategy.ex
@@ -4,6 +4,15 @@ defmodule Usd2rur.CrawlStrategy do
   alias Usd2rur.CrawlStrategy.Sberbank
   alias Usd2rur.CrawlStrategy.Tinkoff
 
+  defmacro __using__(_opts) do
+    quote do
+      alias Usd2rur.CrawlStrategy
+      alias Usd2rur.CrawlStrategy.AlphaBank
+      alias Usd2rur.CrawlStrategy.Sberbank
+      alias Usd2rur.CrawlStrategy.Tinkoff
+    end
+  end
+
   def crawl_map do
     [AlphaBank, Sberbank, Tinkoff]
       |> Enum.reduce(%{}, fn strategy, acc -> Map.put(acc, apply(strategy, :slug, []), strategy) end)
